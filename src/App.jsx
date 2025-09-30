fe3eadbd44e60facac2a0959a8d0ae5fb210e1b7
adrian <adrian.toma006@gmail.com>
2025-09-30 23:04:31 +0100

    Using The Components v1.1

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import { Card } from "./components/Card/Card"
 import './App.css'
 import { Button } from "./components/Button/Button"
 import { Input } from "./components/Input/Input"
+import { useState } from "react"
+
 
 function App() {
-
-
+  const [password, setPassword] = useState("");
+  const handleDelete = (e) => {
+    e.preventDefault();
+    if (password === "password") {
+    alert("Password Deleted Successfully!");
+    } else {
+      alert("Incorrect Password. Please try again.");
+    }
+    setPassword("");
+  }
   return (
     <div className="main">
       <Card variant="shadow">
-        <div className="card-header">User Profile</div>
-        <p>Hello! This is a basic card with some content.</p>
-        <Input placeholder="Enter your name" className="card-input" />
-        <Button variant="primary" className="card-button">Submit</Button>
-        <div className="card-footer">Last updated: Today</div>
+        <div className="card-header">Password Delete</div>
+        <p>Please enter <span style={{ color: "#ff0000" }}>"password"</span> to delete!</p>
+        <Input placeholder="Enter your password" className="card-input" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <Button variant="danger" className="card-button" onClick={handleDelete}>Delete</Button>
+        <div className="card-footer">Last Password update: Today</div>
       </Card>
     </div>
   )
